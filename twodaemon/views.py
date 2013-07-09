befa44c915475db49dfa67bafd1de9d0ca36f7a8
Tom Kitchin <tkitchin@gmail.com>
2013-07-09 14:52:09 +0100

    Add a simple about page.

--- twodaemon/views.py
+++ twodaemon/views.py
@@ -3,24 +3,28 @@
 from flask import render_template, abort
 
 from twodaemon import app
-from twodaemon.article import Article, ArticleNotFoundError, article_list
+from twodaemon import article
 
 @app.route("/")
 def home():
     return render_template('home.html', title="Home")
 
+@app.route("/about")
+def about():
+    return render_template('about.html', title="About")
+
 @app.route("/articles/")
-def articles_list():
-    articles = article_list("article")
+def article_list():
+    articles = article.article_list("article")
     return render_template('articles_list.html', title="Articles", article_list=articles)
 
 @app.route("/articles/<article_name>")
 @app.route("/articles/<article_name>/<int:page_number>")
 def article_single(article_name, page_number=1):
-    article = Article("article", article_name)
+    article_ = article.Article("article", article_name)
     try:
-        article.initialise()
-        page = article.build_page(page_number)
-    except ArticleNotFoundError:
+        article_.initialise()
+        page = article_.build_page(page_number)
+    except article.ArticleNotFoundError:
         abort(404)
-    return render_template('article.html', title=page['article_title'], **page)
+    return render_template('article.html', title=page['page_title'], **page)
